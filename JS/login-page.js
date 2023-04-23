0000a710618e54fb8d8d2699f764a5c2b9ac8adc
Stefaaan <89779209+Stefaaaaaaaaaaaaaaaaaaaaaaaaaaan@users.noreply.github.com>
2023-04-23 10:06:44 +0200

    Semi working Search bar (nur im Shopping cart zum testen halt)

--- JS/login-page.js
+++ JS/login-page.js
@@ -1,16 +1,20 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const loginForm = document.getElementById("login-form");
+document.addEventListener("DOMContentLoaded", function() {  //checks if page is loaded
+    //gets buttons
+    const loginForm = document.getElementById("login-form");  
     const loginButton = document.getElementById("login-form-submit");
     const loginErrorMsg = document.getElementById("login-error-msg");
 
-    loginButton.addEventListener("click", (e) => {
+    loginButton.addEventListener("click", (e) => {    //checks for clicks
       e.preventDefault();
+      //checks input for username and password
       const username = loginForm.username.value;
       const password = loginForm.password.value;
   
-      if (username == "TestUser" && password == "1234") {
+      if (username == "TestUser" && password == "1234") {   
+          //succesfull login
           location.replace("../index.html");
       } else {
+          //failed login
           loginErrorMsg.style.opacity = 1;
       }
     });
---
+++ JS/serarchbar.js
@@ -0,0 +1,22 @@
+
+function search_item() {
+    let input = document.getElementById('search').value
+    input=input.toLowerCase();
+    let x = document.getElementsByClassName('searchItem');
+      
+    for (i = 0; i < x.length; i++) { 
+        if(input.length > 0){
+            if (!x[i].innerHTML.toLowerCase().includes(input)) {
+                x[i].style.display="none";
+                x[i].style.visibility = "hidden";
+            }
+            else {
+                x[i].style.display="list-item";       
+                x[i].style.visibility = "visible";
+            }
+        }else{
+            x[i].style.display="none";
+            x[i].style.visibility = "hidden";
+        }
+    }
+}
