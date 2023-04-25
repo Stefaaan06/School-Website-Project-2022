f33478bc9aabf136f6a77f249e25e2f25de62036
Stefaaan <ste321fan@gmail.com>
2023-04-25 13:37:37 +0200

    search bar updated

--- JS/serarchbar.js
+++ JS/serarchbar.js
@@ -1,22 +1,28 @@
 
 function search_item() {
     let input = document.getElementById('search').value
-    input=input.toLowerCase();
+    input = input.toLowerCase();
+
     let x = document.getElementsByClassName('searchItem');
+    let y = document.getElementById("list");
           
     for (i = 0; i < x.length; i++) { 
         if(input.length > 0){
             if (!x[i].innerHTML.toLowerCase().includes(input)) {
                 x[i].style.display="none";
                 x[i].style.visibility = "hidden";
+                y.style.visibility = "visible";
+                
             }
             else {
                 x[i].style.display="list-item";       
                 x[i].style.visibility = "visible";
+                y.style.visibility = "visible";
             }
         }else{
             x[i].style.display="none";
             x[i].style.visibility = "hidden";
+            y.style.visibility = "hidden";
         }
     }
 }
