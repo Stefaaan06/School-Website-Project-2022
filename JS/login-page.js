bc77917c08f2c61c19d552d8eaf6cb707f0ed48e
Stefaaan <ste321fan@gmail.com>
2023-05-11 13:54:01 +0200

    Dokumentation fertig

--- JS/login-page.js
+++ JS/login-page.js
@@ -1,15 +1,21 @@
-document.addEventListener("DOMContentLoaded", function() {  //checks if page is loaded
-    //gets buttons
+/*
+Login System
+@author Stefan Crasninec
+@version 11.05.2023
+ */
+document.addEventListener("DOMContentLoaded", function() {  //wartet bis die Website geladen ist
+    //findet die gesuchten Elemente welche zum Login verwendet werden
     const loginForm = document.getElementById("login-form");  
     const loginButton = document.getElementById("login-form-submit");
     const loginErrorMsg = document.getElementById("login-error-msg");
 
-    loginButton.addEventListener("click", (e) => {    //checks for clicks
+    loginButton.addEventListener("click", (e) => {    //wartet auf clicks auf den Login Button
       e.preventDefault();
-      //checks input for username and password
+      //nimmt sich die eingegebenen Daten aus den Feldern herraus
       const username = loginForm.username.value;
       const password = loginForm.password.value;
-  
+
+      //vergleicht Daten mit Account
       if (username == "TestUser" && password == "1234") {   
           //succesfull login
           location.replace("../index.html");
--- JS/searchbar.js
+++ JS/searchbar.js
@@ -1,26 +1,36 @@
-
+/*
+Item suche
+@author Stefan Crasninec
+@version 11.05.2023
+ */
 function search_item() {
-    let input = document.getElementById('search').value
-    input = input.toLowerCase();
+    let input = document.getElementById('search').value     //nimmt sich den Input hinaus
+    input = input.toLowerCase();    //macht den input zu lower case
 
+    //holt sich die Itemliste & die einzelnen Items
     let x = document.getElementsByClassName('searchItem');
     let y = document.getElementById("list");
-          
-    for (i = 0; i < x.length; i++) { 
+
+    //wiederholt sich für jedes Item
+    for (i = 0; i < x.length; i++) {
+        //falls der Input größer als 1 ist
         if(input.length > 1){
-            if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            if (!x[i].innerHTML.toLowerCase().includes(input)) {    //falls kein Item gefunden wurde
+                //versteckt element
                 x[i].style.display="none";
                 x[i].style.visibility = "hidden";
                 y.style.visibility = "visible";
                 y.style.display="initial"
-                
+
             }
-            else {
-                x[i].style.display="list-item";       
+            else {  //falls ein Item gefunden wurde
+                //zeigt element
+                x[i].style.display="list-item";
                 x[i].style.visibility = "visible";
                 y.style.visibility = "visible";
             }
-        }else{
+        }else{  //falls kein oder nicht genug charaktere eingegeben wurde
+            //versteckt elemente
             x[i].style.display="none";
             x[i].style.visibility = "hidden";
             y.style.visibility = "hidden";
